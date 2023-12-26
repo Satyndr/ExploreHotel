2941c34a9fcc416c5f1b0fd012f593dc4d1fa741
Satendra kumar <satyendrakumar.contact@gmail.com>
2023-12-26 05:40:40 +0530

    login and signup form and functionality added

--- app.js
+++ app.js
@@ -6,9 +6,13 @@ const ejsMate = require("ejs-mate");
 const ExpressError = require("./utils/ExpressError.js");
 const session = require("express-session");
 const flash = require("connect-flash");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user.js");
 
 const listings = require("./routes/listing.js")
 const reviews = require("./routes/review.js")
+const userRouter = require("./routes/user.js");
 
 const app = express();
 
@@ -54,6 +58,13 @@ app.get("/", (req,res)=>{
 app.use(session(sessionOptions));
 app.use(flash());
 
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 app.use((req, res, next)=>{
     res.locals.success = req.flash("success");
     res.locals.error = req.flash("error");
@@ -64,6 +75,7 @@ app.use((req, res, next)=>{
 
 app.use("/listings", listings);
 app.use("/listings/:id/reviews", reviews);
+app.use("/", userRouter);
 
 //error handling middleware---------------------------------------
 app.all("*", (req,res,next)=>{
